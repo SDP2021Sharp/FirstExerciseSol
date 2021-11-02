dec69c864b4d2330cc7be5a463911afa01f1d35f
michu3136 <michu3136@gmail.com>
2021-11-02 10:15:18 +0100

    Add project files.

---
+++ src/GitExercise1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitExercise1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
